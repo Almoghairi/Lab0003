5c23b79f7585e658e2a786313485c5ae8b727f9a
Almoghairi <abdulazizalmoghairi@outlook.com>
2024-02-11 23:43:33 +0300

    sort2
    update

--- src/Sorter.java
+++ src/Sorter.java
@@ -15,4 +15,23 @@ public class Sorter {
             arr[i] = smallerNumber;
         }
     }
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+    public static void sort(int[] array) {
+        insertionSort(array);
+    }
+    public static void sort2(int[] array) {
+        selectionSort(array);
+    }
 }
